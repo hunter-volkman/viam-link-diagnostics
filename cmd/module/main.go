fd6414a8769dc55055e96e420471ea46bcd296a8
hunter-volkman <huntervolkman@protonmail.com>
2025-09-12 17:27:45 -0400

    Fix module

--- cmd/module/main.go
+++ cmd/module/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	link_diagnostics "link-diagnostics"
+
 	"go.viam.com/rdk/components/sensor"
 	"go.viam.com/rdk/module"
 	"go.viam.com/rdk/resource"
 )
 
 func main() {
-	module.ModularMain(resource.APIModel{sensor.API, link - diagnostics.Model})
+	module.ModularMain(resource.APIModel{sensor.API, link_diagnostics.Model})
 }
